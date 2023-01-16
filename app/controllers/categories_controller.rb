0899c0b8b01d3c98abc0f1742886a8d04fbc6b19
Deepti Jain <deeptidj@gmail.com>
2023-01-16 11:34:39 -0800

    more

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,25 +1,59 @@
 class CategoriesController < ApplicationController
+
   def index
+    @categories = Category.order(:name)
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def new
+    @count = Category.count
+    @category = Category.new(position: @count + 1)
   end
 
   def create
+    @category = Category.new(categories_params)
+    if @category.save
+      redirect_to
+    else
+      # The 'new' action is NOT being called here
+      # Assign any instance variables needed
+      # @count = Task.count
+      render('new')
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update(task_params)
+      redirect_to task_path(@category)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @category = Category.find(params[:id])
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
   end
+
+  private
+
+  def categories_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
